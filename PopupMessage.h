8702a6406dc0439e4d9d00ae89366d3c48956d50
Hastyar1 <114627830+Hastyar1@users.noreply.github.com>
2024-02-09 11:22:56 +0300

    Update and rename PopupViewController.h to PopupMessage.h

---
+++ PopupMessage.h
@@ -0,0 +1,12 @@
+// PopupMessage.h
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PopupMessage : NSObject
+
++ (void)showPopupWithTitle:(NSString *)title message:(NSString *)message;
+
+@end
+
+NS_ASSUME_NONNULL_END
